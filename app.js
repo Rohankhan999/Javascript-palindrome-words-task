d53504526a6de8d64010ea1a1d576448f7f8b05f
Rohan <Rohankhan08453@gmail.com>
2024-09-20 11:50:37 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,21 @@ if(userInput === result){
     alert("Enter Palendrome word!!!!!!!!!!!!!!")
 }
 
+// WITHOUT METHOD!
+
+// function reverseString(str) {
+//     const stack = [];
+//     // Push each character onto the stack
+//     for (let char of str) {
+//       stack.push(char);
+//     }
+//     // Pop each character from the stack to reverse the string
+//     let reversed = '';
+//     while (stack.length > 0) {
+//       reversed += stack.pop();
+//     }
+//     return reversed;
+//   }
+  
+//   document.write(reverseString('NAHOR'));
+  
