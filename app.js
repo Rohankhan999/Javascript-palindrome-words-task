5c46d47e57f7a5f7c92c24c61060677313b56473
Rohankhan999 <159823468+Rohankhan999@users.noreply.github.com>
2024-09-22 09:21:44 +0500

    Update app.js

--- app.js
+++ app.js
@@ -7,20 +7,20 @@ if(userInput === result){
 }
 
 // WITHOUT METHOD!
+// var word = prompt ("Please Enter a word");
+// var newword = ""
+// document.write("Input Word: " + word + "<br>")
 
-// function reverseString(str) {
-//     const stack = [];
-//     // Push each character onto the stack
-//     for (let char of str) {
-//       stack.push(char);
-//     }
-//     // Pop each character from the stack to reverse the string
-//     let reversed = '';
-//     while (stack.length > 0) {
-//       reversed += stack.pop();
-//     }
-//     return reversed;
-//   }
-  
-//   document.write(reverseString('NAHOR'));
-  
+// for (let i = word.length-1; i >=0; i--) {
+//     newword += word[i]
+
+// }
+// document.write("Reverse Word: " + newword)
+
+//     if (newword=== word) { 
+//         document.write("<br> It is a Pallindrome")
+        
+//     }else{
+
+//         document.write("<br> It is a Not a Pallindrome")
+//     } 
